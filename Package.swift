7e78e40c28b5447e524dd3d9173bbac848da3ec2
Martynas Limontas <martynas@jumsoft.com>
2021-03-31 11:01:44 +0300

    spm test

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "saltedge-ios-swift-BlixLT",
-            targets: ["saltedge-ios-swift-BlixLT"]),
+            name: "saltedge-ios-swift",
+            targets: ["saltedge-ios-swift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,12 +23,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "saltedge-ios-swift-BlixLT",
+            name: "saltedge-ios-swift",
             dependencies: [],
             path: "saltedge-ios-swift"),
         .testTarget(
             name: "saltedge-ios-swift-BlixLTTests",
-            dependencies: ["saltedge-ios-swift-BlixLT"],
+            dependencies: ["saltedge-ios-swift"],
             path: "Example/Tests"),
     ]
 )
